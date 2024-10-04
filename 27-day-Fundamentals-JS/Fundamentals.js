12989fec6cab3910e5c2546428d02bfa8b11e72f
Aafia <aafiabhatti23@gmail.com>
2024-10-04 14:03:27 +0500

    27Day-Fundamentals-(variables.values.datatypes)-JS

---
+++ 27-day-Fundamentals-JS/Fundamentals.js
@@ -0,0 +1,89 @@
+// Values
+console.log("Values in JS");
+
+// string
+console.log("String==> ","Javascript is fun");
+
+// numbers
+console.log("Number==> ",22);
+
+// Booleans
+console.log("Boolean==> ",true);
+console.log("Boolean==> ",false);
+
+// Null
+
+console.log("Null==>",null);
+
+// undefines
+console.log("Undefined==>",undefined);
+
+
+// Datatypes in JS - 7
+
+// 1-String
+// 2-Number
+// 3-Boolean
+// 4-Null
+// 5-Undefined
+// 6-Symbol
+// 7-BigInt
+
+
+// Variables in JS 
+console.log("Datatypes in js");
+
+age=22;
+console.log("Age==> ", age);
+console.log("Type of age==>",typeof age);
+
+age="23";
+console.log("Age==> ", age);
+console.log("Type of age==>",typeof age);
+
+// Keywords
+// 1-var
+// 2-let
+// 3-const
+
+var numOne= 100;
+
+let numTwo=100;
+
+const numThree=100;
+
+console.log(numOne,numTwo,numThree);
+console.log(typeof numOne,typeof numTwo,typeof numThree);
+
+console.log("Variables in JS");
+// 1- Declaration
+
+var aNumber;
+
+// 2-Assignment / initialization
+
+aNumber=22;
+
+// Difference
+console.log("Variable with keyword var");
+
+var age;
+age=22;
+
+var age;
+age=23;
+console.log("Age==> ",age);
+
+console.log("Variable with keyword let");
+let gender;
+gender="male";
+console.log("Gender==> ",gender);
+
+gender="female";
+console.log("Gender==> ",gender);
+
+console.log("Variable with keyword const");
+const city="Burewala";
+console.log("City==> ",city);
+
+
